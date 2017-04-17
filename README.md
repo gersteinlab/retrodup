1b389ce9e0c94ac38ea4498debb5121c4039d739
Shantao Li of group gerstein <sl847@farnam1.farnam.hpc.yale.internal>
2017-04-16 21:29:30 -0400

    Fixed readme

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ Please refer to /*Landscape and Variation of Novel Retroduplications in 26 Human
 
 
 1. Create junction libraries
+`create_junction_library.pl -m null_mode -g genome_dir annotation_file (gencode)`
 2. Extract unmapped reads and re-map to junctions
+`run_bwa.sh -o output_suffix junction_libs bam_files >& out.txt`
 3. Call retroduplications from exon-exon junction
+`call_retroduplications.pl annotation_file sam_file > result.txt`
 4. Cluster the discordant reads to discover insertion sites	
+`samtools view bamfile gene_coor | java Cluster gene_coor2 > insertionpoints.txt`
--- create_junction_library.pl
+++ create_junction_library.pl
@@ -17,19 +17,19 @@ my $NO_ID  = "__NO_GENE_ID__";
 my $EXTEND = 100;
 
 my $gencode_file       = "";
-my $mode               =  0; # predefined for true junction lib
-my ($shift_s,$shift_e) = (0,0); # predefined for true junction lib
+my $mode               =  0; 
+my ($shift_s,$shift_e) = (0,0); 
 my $genome_dir         = "./";
 
-my $n_arg = scalar(@ARGV); # number of parameters
-for (my $i = 0;$i < $n_arg;$i++) { # get parameters of create_junction_library.pl
+my $n_arg = scalar(@ARGV);
+for (my $i = 0;$i < $n_arg;$i++) { 
     my $arg = $ARGV[$i];
     if ($arg eq "-m") {
 	if (++$i < $n_arg) { $mode       = $ARGV[$i]; }
     } elsif ($arg eq "-g") {
-	if (++$i < $n_arg) { $genome_dir = $ARGV[$i]; } # dir updated
+	if (++$i < $n_arg) { $genome_dir = $ARGV[$i]; } 
     } else {
-	if (length($gencode_file) > 0) { # predefined par. above
+	if (length($gencode_file) > 0) {
 	    print STDERR "Umbiguous input.\n";
 	    print STDERR $usage,"\n";
 	    exit;
@@ -38,7 +38,7 @@ for (my $i = 0;$i < $n_arg;$i++) { # get parameters of create_junction_library.p
     }
 }
 
-if ($mode =~ m/^(\d+)\-(\d+)$/) { # check mode par. format
+if ($mode =~ m/^(\d+)\-(\d+)$/) { 
     $shift_s =  $1;
     $shift_e = -$2;
 } elsif ($mode =~ m/^(\-\d+)$/) {
@@ -51,14 +51,14 @@ if ($mode =~ m/^(\d+)\-(\d+)$/) { # check mode par. format
     exit;
 }
 
-if (length($gencode_file) == 0) { # check gencode_file par. format
+if (length($gencode_file) == 0) { 
     print STDERR "No annotation file is provided.\n";
     print STDERR $usage,"\n";
     exit;
 }
 
 my @genes              = ();
-my %gene_exons         = (); # hash for gene_exons
+my %gene_exons         = (); 
 my $FILE = RETRODUP::openFile($gencode_file);
 if (!$FILE) { exit; }
 
@@ -69,13 +69,13 @@ while (my $line = <$FILE>) {
     if ($w[2] eq "gene") {
 	my $id = getId($w[8]);
 	my @tmp = (getChromosome($w[0]));
-	$gene_exons{$id} = \@tmp; # $id is the hash key. left-side now holds a reference to @tmp
-	push(@genes,$id); # store geneID in @genes
+	$gene_exons{$id} = \@tmp; 
+	push(@genes,$id); 
     } elsif ($w[2] eq "exon") {
 	my $id = getId($w[8]);
-	my $arr = $gene_exons{$id}; # reference to @tmp, whose value is chromosome?
+	my $arr = $gene_exons{$id}; 
 	if (!$arr) { next; }
-	push(@$arr,$w[3] + $shift_s); # shift coordinates according to null model
+	push(@$arr,$w[3] + $shift_s);
 	push(@$arr,$w[4] + $shift_e);
     }
 }
@@ -84,8 +84,8 @@ close($FILE);
 my @junctions      = ();
 my %junction_names = ();
 
-foreach my $gene (@genes) { # generate junctions and junction names?
-    my $arr = $gene_exons{$gene}; # information: chr, shifted coordinates
+foreach my $gene (@genes) { 
+    my $arr = $gene_exons{$gene};
     my $n = scalar(@$arr);
     if ($n <= 3) { next; } # if single exon, ignored
     if (int($n/2)*2 + 1 != $n) { next; }
@@ -103,8 +103,6 @@ foreach my $gene (@genes) { # generate junctions and junction names?
     for (my $ie = 0;$ie < $ne;$ie++) {
 	my $s = $ee[$ie];
 	my $prefix = $c.":".($s - $EXTEND + 1)."-".$s."|0|"; 
-	                         # EXTEND def: take length EXTEND in each of the two exons, then merge these two of length EXTEND.
-	
 	for (my $is = 0;$is < $ns;$is++) {
 	    my $e = $ss[$is];
 	    if ($e <= $s) { next; }
@@ -121,7 +119,7 @@ foreach my $gene (@genes) { # generate junctions and junction names?
 }
 
 my ($seq,$chrom) = ("","");
-foreach my $j (@junctions) { # output result into file
+foreach my $j (@junctions) {  
     my ($coor1,$ins,$coor2) = split(/\|/,$j);
     my ($c1,$s1,$e1) = split(/[\:\-]/,$coor1);
     my ($c2,$s2,$e2) = split(/[\:\-]/,$coor2);
@@ -132,7 +130,7 @@ foreach my $j (@junctions) { # output result into file
     my $seq1 = substr($seq,$s1 - 1,$e1 - $s1 + 1);
     if ($c2 ne $chrom) {
 	$chrom = $c2;
-	$seq = parseChromosome($c2); # match to ref genome to extract sequence?
+	$seq = parseChromosome($c2); 
     }
     my $seq2 = substr($seq,$s2 - 1,$e2 - $s2 + 1);
     print ">",$junction_names{$j},"|splj|",$j,"|0\n";
@@ -182,7 +180,7 @@ sub getId # return 'gene_id'_'gene_name'
     return $ret;
 }
 
-sub getChromosome # keep only the number, remove 'chr'
+sub getChromosome 
 {
     my $in = shift;
     if (substr($in,0,3) eq "chr") { return substr($in,3); }
